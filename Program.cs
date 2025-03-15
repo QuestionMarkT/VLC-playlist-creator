a19bc1e2b2e373e23ec267f0f561e93c972b0fbf
? <LP3CG@HOTMAIL.COM>
2025-03-15 14:41:13 +0100

    Basic functionality

--- Program.cs
+++ Program.cs
@@ -1,37 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
-namespace VLC_playlist_creator
+namespace VLC_playlist_creator;
+
+internal class Program
 {
-    internal class Program
+    static readonly string[] possibleSwitches = ["/D", "/S", "/T"];
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+       /*
+        * /S C:\Users\Administrator\Music\
+        * /D Y:\Safe\Start Menu\VLC\
+        * /N Custom generated playlist
+        * /T 90
+        * 
+        */
+        if(ArgsInvalid(out string message, out Dictionary<string, string> convertedArgs, args))
         {
-            if(ArgsInvalid(out string message, args))
-            {
-                Console.WriteLine(message);
-                return;
-            }
+            Console.WriteLine(message);
+            return;
         }
 
-        static bool ArgsInvalid(out string message, params string[] args)
+        if(!Directory.Exists(convertedArgs["/S"]))
         {
-            if(args.Length == 0)
+            Console.WriteLine("Folder not found " + convertedArgs["/S"]);
+        }
+    }
+
+    /// <summary>
+    /// Checks if provided arguments are invalid
+    /// </summary>
+    /// <param name="message">if it returns</param>
+    /// <param name="dict">sdf</param>
+    /// <param name="args">dfgdfg</param>
+    /// <returns><see langword="true"/> if invalid, <see langword="false"/> if valid</returns>
+    static bool ArgsInvalid(out string message, out Dictionary<string, string> dict, params string[] args)
+    {
+        dict = [];
+        if(args.Length == 0)
+        {
+            message = "No arguments have been specified. Use /? for help.";
+            return true;
+        }
+
+        if(args[0] == "/?")
+        {
+            message = "/S\tsource location where all your music is located, root directory e.g. " + @"C:\Users\Administrator\Music\" + Environment.NewLine +
+                "/D\tdestination location where you want .xspf playlist files to appear" + Environment.NewLine +
+                "/T\ttime in days, how many last days to check for to generate recent.xspf playlist";
+
+            return true;
+        }
+
+        if(args.Length % 2 != 0)
+        {
+            message = "Parameters number must be even.";
+            return true;
+        }
+
+        if(args.Length > possibleSwitches.Length * 2)
+        {
+            message = $"Too many arguments provided: {args.Length} but the limit is {possibleSwitches.Length * 2}.";
+            return true;
+        }
+
+        for(byte x = 0 ; x < args.Length ; x += 2)
+        {
+            string param = args[x].ToUpper();
+
+            if(!possibleSwitches.Any(R => R == param))
             {
-                message = "No arguments have been specified. Use /? for help";
+                message = "Unrecognized switch: " + args[x];
                 return true;
             }
 
-            if(args[0] == "/?")
+            if(!dict.TryAdd(param, args[x + 1]))
             {
-
+                message = "Duplicate parameters or parameters specified incorrectly.";
+                return true;
             }
-
-            foreach(string arg in args)
-            {
-
-            }
-
         }
+        
+        message = string.Empty;
+        return false;
     }
 }

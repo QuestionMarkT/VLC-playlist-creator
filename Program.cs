427920184e9af43e7661d0a8b2f16f978acad704
? <LP3CG@HOTMAIL.COM>
2025-03-15 12:43:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace VLC_playlist_creator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(ArgsInvalid(out string message, args))
+            {
+                Console.WriteLine(message);
+                return;
+            }
+        }
+
+        static bool ArgsInvalid(out string message, params string[] args)
+        {
+            if(args.Length == 0)
+            {
+                message = "No arguments have been specified. Use /? for help";
+                return true;
+            }
+
+            if(args[0] == "/?")
+            {
+
+            }
+
+            foreach(string arg in args)
+            {
+
+            }
+
+        }
+    }
+}
